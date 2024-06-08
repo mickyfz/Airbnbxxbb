cdf38fa173823f2b61cb86f6ddce5983ee0f4a77
Micky Fazler <57700946+mickyfazler@users.noreply.github.com>
2024-06-08 18:17:52 +0600

    post with props baby

--- App.js
+++ App.js
@@ -4,15 +4,20 @@ import {View, Text, StyleSheet,Button, TextInput,SafeAreaView} from 'react-nativ
 
 import HomeScreenbb from "./srcy/screensy/HomeScreeny";
 import Postbb from "./srcy/componentsy/Posty";
+import feedbb from "./assetsy/data/feed";
 const App = () => {
 // const App: () => React$Node = () => {
 
-
+const feed=feedbb;
+// console.log(feed);
 return (
   <>
   <SafeAreaView>
     {/* <HomeScreenbb/> */}
+    <Postbb feedy={feed}/>
+    {/* <Postbb/>
     <Postbb/>
+    <Postbb/> */}
   </SafeAreaView>
   </>
   )
--- srcy/componentsy/Posty/index.js
+++ srcy/componentsy/Posty/index.js
@@ -2,16 +2,17 @@ import { View, Text, Image } from 'react-native'
 import React from 'react'
 import stylesbb from './stylesy'
 
-const Postbb = () => {
+const Postbb = (propsy) => {
+  const propbb=propsy['feedy'][2];
+  console.log('propy',propbb);
   return (
     <View style={stylesbb.containery}>
       {/* SECTION:  Image  */}
-      <Image source={{uri:'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg'}} style={stylesbb.img}/>
+      <Image source={{uri:propbb.image}} style={stylesbb.img}/>
 
       {/* SECTION:   Bed & Bedroom  */}
       <Text style={stylesbb.bedroms}>
-        <Text>1 bed </Text> 
-        <Text>1 bedroom</Text> 
+        <Text>{propbb.bed} bed {propbb.bedroom} bedroom</Text> 
       </Text>
 
       {/* SECTION:   Type & Description */}
@@ -19,20 +20,54 @@ const Postbb = () => {
       <Text style={stylesbb.descrips} 
       numberOfLines={2}   // 2 lines will be shown.
       
-      >Entire flat • Here we have lots of text, which you won't ever read them. I'm just saying, just bluffing, whatever I'm doing, you won't read it. Just saying, bro,</Text>
+      >{propbb.type} • {propbb.title}</Text>
      
       {/* SECTION:    Old price & new price */}
        {/*  We wrapping Texts by a Text not a View. Because it put all the text in on one line. */}
       <Text style={stylesbb.prices}>   
-        <Text style={stylesbb.oldPrice}>$43</Text>
-        <Text style={stylesbb.newPrice}> $36 </Text>
+        <Text style={stylesbb.oldPrice}>${propbb.oldPrice}</Text>
+        <Text style={stylesbb.newPrice}> ${propbb.newPrice} </Text>
         / night
       </Text>
 
       {/*  SECTION:   Total price */}
-      <Text style={stylesbb.totalPrice}> $247 total </Text>
+      <Text style={stylesbb.totalPrice}> ${propbb.totalPrice} total </Text>
     </View>
   )
 }
+
+// Static data
+// const Postbb = () => {
+//   return (
+//     <View style={stylesbb.containery}>
+//       {/* SECTION:  Image  */}
+//       <Image source={{uri:'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg'}} style={stylesbb.img}/>
+
+//       {/* SECTION:   Bed & Bedroom  */}
+//       <Text style={stylesbb.bedroms}>
+//         <Text>1 bed </Text> 
+//         <Text>1 bedroom</Text> 
+//       </Text>
+
+//       {/* SECTION:   Type & Description */}
+
+//       <Text style={stylesbb.descrips} 
+//       numberOfLines={2}   // 2 lines will be shown.
+      
+//       >Entire flat • Here we have lots of text, which you won't ever read them. I'm just saying, just bluffing, whatever I'm doing, you won't read it. Just saying, bro,</Text>
+     
+//       {/* SECTION:    Old price & new price */}
+//        {/*  We wrapping Texts by a Text not a View. Because it put all the text in on one line. */}
+//       <Text style={stylesbb.prices}>   
+//         <Text style={stylesbb.oldPrice}>$43</Text>
+//         <Text style={stylesbb.newPrice}> $36 </Text>
+//         / night
+//       </Text>
+
+//       {/*  SECTION:   Total price */}
+//       <Text style={stylesbb.totalPrice}> $247 total </Text>
+//     </View>
+//   )
+// }
 
 export default Postbb
